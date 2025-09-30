8b7ef6a811b263fbe0475aeaeb7b705e39065a50
nekkaida <kennethriadinugroho@gmail.com>
2025-09-30 11:44:43 +0800

    feat(auth): export legal modal components
    - Add exports for LegalModal, PrivacyPolicyModal, TermsOfServiceModal
    - Centralize auth component exports

---
+++ src/features/auth/components/index.ts
@@ -0,0 +1,7 @@
+// Auth Components
+export * from './AuthComponents';
+
+// Legal Modals
+export * from './LegalModal';
+export * from './PrivacyPolicyModal';
+export * from './TermsOfServiceModal';
