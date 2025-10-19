4ef6db68e849707bd7f6be943d2b815a8dbc9719
nekkaida <kennethriadinugroho@gmail.com>
2025-10-19 14:08:31 +0800

    feat(profile): export new profile components

--- src/features/profile/components/index.ts
+++ src/features/profile/components/index.ts
@@ -1,3 +1,4 @@
+// Legacy components (may be outdated)
 export { DropdownModal } from './DropdownModal';
 export { InlineDropdown } from './InlineDropdown';
 export { WinRateCircle } from './WinRateCircle';
@@ -6,3 +7,9 @@ export { EloProgressGraph } from './EloProgressGraph';
 export { EditIcon } from './EditIcon';
 export { MatchHistoryButton } from './MatchHistoryButton';
 export { AchievementIcon } from './AchievementIcon';
+
+// New refactored components (match current profile.tsx UI)
+export { ProfileHeaderWithCurve } from './ProfileHeaderWithCurve';
+export { ProfilePictureSection } from './ProfilePictureSection';
+export { ProfileInfoCard } from './ProfileInfoCard';
+export { ProfileAchievementsCard } from './ProfileAchievementsCard';
