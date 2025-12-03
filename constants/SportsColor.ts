a02e1d2d18ee63d32cca24b35751b6c17695642a
Zawad Wasik Ahmed <69716177+ZawadAhmed@users.noreply.github.com>
2025-12-03 15:45:39 +0600

    Refactor sports color

--- constants/SportsColor.ts
+++ constants/SportsColor.ts
@@ -4,6 +4,8 @@ export interface SportColors {
   background: string;
   badgeColor: string;
   label: string;
+  buttonColor: string;
+  icon: string;
 }
 
 export const SPORT_COLORS: Record<SportType, SportColors> = {
@@ -11,16 +13,22 @@ export const SPORT_COLORS: Record<SportType, SportColors> = {
     background: '#863A73',
     badgeColor: '#A855F7',
     label: 'PICKLEBALL',
+    buttonColor: '#602E98',
+    icon: 'disc',
   },
   TENNIS: {
     background: '#65B741',
     badgeColor: '#22C55E',
     label: 'TENNIS',
+    buttonColor: '#587A27',
+    icon: 'tennisball',
   },
   PADEL: {
     background: '#3B82F6',
     badgeColor: '#60A5FA',
     label: 'PADEL',
+    buttonColor: '#2E6698',
+    icon: 'tennisball',
   },
 };
 
@@ -28,7 +36,7 @@ export const getSportColors = (
   sportType: SportType | null | undefined
 ): SportColors => {
   if (!sportType) {
-    return SPORT_COLORS.PICKLEBALL; // Default
+    return SPORT_COLORS.PICKLEBALL;
   }
   return SPORT_COLORS[sportType] || SPORT_COLORS.PICKLEBALL;
 };
