d96097dfd117fbffb0e350eb1bdb2222f9dbe452
nekkaida <kennethriadinugroho@gmail.com>
2025-12-18 13:46:45 +0800

    chore: add jest configuration files

---
+++ jest.config.js
@@ -0,0 +1,48 @@
+/** @type {import('jest').Config} */
+module.exports = {
+  preset: 'jest-expo/ios',
+  roots: ['<rootDir>/src', '<rootDir>/app'],
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+    '**/*.test.[jt]s?(x)',
+    '**/*.spec.[jt]s?(x)',
+  ],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@features/(.*)$': '<rootDir>/src/features/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
+    '^@core/(.*)$': '<rootDir>/src/core/$1',
+    '^@hooks/(.*)$': '<rootDir>/src/hooks/$1',
+    '^@services/(.*)$': '<rootDir>/src/services/$1',
+    '^@utils/(.*)$': '<rootDir>/src/utils/$1',
+    '^@constants/(.*)$': '<rootDir>/constants/$1',
+    '^@lib/(.*)$': '<rootDir>/lib/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  transformIgnorePatterns: [
+    'node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|@gorhom/.*)',
+  ],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/**/index.ts',
+    '!src/**/*.test.{ts,tsx}',
+    '!src/**/__tests__/**',
+  ],
+  coverageThreshold: {
+    global: {
+      branches: 5,
+      functions: 5,
+      lines: 5,
+      statements: 5,
+    },
+  },
+  coverageReporters: ['text', 'text-summary', 'lcov', 'html'],
+  testTimeout: 10000,
+  clearMocks: true,
+  resetMocks: true,
+  globals: {
+    __DEV__: true,
+  },
+};
