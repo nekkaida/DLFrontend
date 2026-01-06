e4c0a4f6259209188f77b5bec093a10ab6165d15
nekkaida <kennethriadinugroho@gmail.com>
2026-01-06 18:02:57 +0800

    feat(feed): add feature barrel export

---
+++ src/features/feed/index.ts
@@ -0,0 +1,7 @@
+// src/features/feed/index.ts
+
+export { default as FeedScreen } from './screens/FeedScreen';
+export * from './components';
+export * from './hooks';
+export * from './types';
+export { feedTheme } from './theme';
