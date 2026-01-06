7e34d9cdfa93575b299707763f74275863b56b10
nekkaida <kennethriadinugroho@gmail.com>
2026-01-06 18:02:42 +0800

    feat(feed): add components barrel export

---
+++ src/features/feed/components/index.ts
@@ -0,0 +1,7 @@
+// src/features/feed/components/index.ts
+
+export { AuthorHeader } from './AuthorHeader';
+export { SocialBar } from './SocialBar';
+export { FeedPostCard } from './FeedPostCard';
+export { FeedHeader } from './FeedHeader';
+export { CommentsSheet } from './CommentsSheet';
